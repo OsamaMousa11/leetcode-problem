8a87ddff904313cfe7bbada895b9a443a18658a9
OsamaMousa11 <osamaomar1995@gmail.com>
2025-06-12 15:17:16 +0300

    Add project files.

---
+++ 13. Roman to Integer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+       Console.WriteLine( RomanToInt("MCMXCIV"));
+      
+    }
+
+    public  static int RomanToInt(string s)
+    {
+        Dictionary<char, int> word = new Dictionary<char, int>();
+
+        word['I'] = 1;
+        word['V'] = 5;
+        word['X'] = 10;
+        word['L'] = 50;
+        word['C'] = 100;
+        word['D'] = 500;
+        word['M'] = 1000;
+        var sum = 0;
+        var oldvalue = 0;
+
+        foreach (var i in s)
+        {
+          int CurrentValue = word[i];
+           sum+=(CurrentValue >oldvalue) ? CurrentValue-(oldvalue*2) : CurrentValue;
+            oldvalue = CurrentValue;
+        }
+        return sum; 
+    }
+}
+
